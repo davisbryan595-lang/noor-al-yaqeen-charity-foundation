08f6562b17f4b743d1d4c658939f23bd67db945b
Wasif <rwasif280@gmail.com>
2025-09-19 05:37:02 +0500

    Update navigation.tsx

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -18,13 +18,13 @@ export function Navigation() {
     <nav className="fixed top-0 left-0 right-0 z-50 bg-background/95 backdrop-blur-sm border-b border-border">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
-          {/* Logo only */}
+          {/* Logo */}
           <div className="flex items-center">
             <Image
               src="/logo.jpg"
               alt="ShareTheMeal Logo"
-              width={40}
-              height={40}
+              width={50} // Increased size
+              height={50} // Increased size
               className="rounded-full"
             />
           </div>
